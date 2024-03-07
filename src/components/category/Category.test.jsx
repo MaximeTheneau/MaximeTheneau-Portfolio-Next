3cf69617fa409361b99049e5284769f010df7cb5
Theneau Maxime <theneau.maxime@gmail.com>
2024-03-07 17:40:20 +0100

    Feat change css index

--- src/components/category/Category.test.jsx
+++ src/components/category/Category.test.jsx
@@ -1,29 +1,17 @@
-import React from 'react';
 import {
   render, waitFor, screen, fireEvent,
 } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { act } from 'react-dom/test-utils';
 import Category from './Category';
 
 describe('Category', () => {
-  test('renders category with "Articles" link', () => {
-    // Ciblez le lien en utilisant le rôle "link" et l'attribut data-testid
-    //  const articlesLinkElement = screen.getByText(/Articles/i);
+  it('renders category with "Articles" link', () => {
+    render(<Category />);
 
-    // const articlesLinkElement = getByTestId('category-link');
-    // expect(articlesLinkElement).toBeInTheDocument();
-    // expect(articlesLinkElement.tagName.toLowerCase()).toBe('a');
-    // expect(articlesLinkElement.getAttribute('href')).toBe('/articles');
-
-    act(() => {
-      render(<Category />);
-
-      waitFor(() => {
-        const linkArticles = screen.queryByTestId('articles-link');
-        expect(linkArticles).toBeInTheDocument();
-        fireEvent.click(screen.queryByTestId('articles-link'));
-      });
+    waitFor(() => {
+      const linkArticles = screen.queryByTestId('articles-link');
+      expect(linkArticles).toBeInTheDocument();
+      fireEvent.click(screen.queryByTestId('articles-link'));
     });
   });
 });
--- src/components/contact/Contact.test.jsx
+++ src/components/contact/Contact.test.jsx
@@ -1,35 +1,47 @@
-/* eslint-disable */
 import {
-  render, cleanup, fireEvent, act,
+  render, fireEvent, act, waitFor,
 } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { useRouter } from 'next/router';
-import fetch from 'node-fetch';
 import FormContact from './Contact';
 
-global.fetch = fetch;
-
-afterEach(cleanup);
+global.fetch = jest.fn(() => Promise.resolve({ ok: true }));
 
 jest.mock('next/router', () => ({
   useRouter: jest.fn(),
 }));
 
-test('submits the form', async () => {
-  useRouter.mockReturnValue({
-    push: jest.fn(),
-  });
-  const { getByTitle, getByText } = render(<FormContact onSubmit={() => {}} handleOnMouseEnter={() => {}} />);
+describe('FormContact', () => {
+  test('submits the form', async () => {
+    useRouter.mockReturnValue({
+      push: jest.fn(),
+    });
+    const { getByTitle, getByText } = render(
+      <FormContact onSubmit={() => {}} handleOnMouseEnter={() => {}} />,
+    );
 
-  await act(async () => {
-    fireEvent.change(getByTitle('Nom'), { target: { value: 'Test Message site' } });
-    fireEvent.change(getByTitle('Email'), { target: { value: 'test@exemple.fr' } });
-    fireEvent.change(getByTitle('Sujet'), { target: { value: 'Test' } });
-    fireEvent.change(getByTitle('Message'), { target: { value: 'Test email Ok :) !' } });
+    await act(async () => {
+      fireEvent.change(getByTitle('Nom'), { target: { value: 'Test Message site' } });
+      fireEvent.change(getByTitle('Email'), { target: { value: 'test@exemple.fr' } });
+      fireEvent.change(getByTitle('Sujet'), { target: { value: 'Test' } });
+      fireEvent.change(getByTitle('Message'), { target: { value: 'Test email Ok :) !' } });
 
-    const submitButton = getByText('Envoyer');
-    fireEvent.click(submitButton);
-
-    await new Promise((resolve) => setTimeout(resolve, 500));
-  });
+      const submitButton = getByText('Envoyer');
+      fireEvent.click(submitButton);
+      await waitFor(() => {
+        // Vérifiez que le code de réponse est celui attendu
+        expect(fetch).toHaveBeenCalledWith(
+          expect.objectContaining({
+            body: JSON.stringify({
+              name: 'Test Message site',
+              email: 'test@exemple.fr',
+              message: 'Test email Ok :) !',
+              subject: 'Test',
+            }),
+            method: 'POST',
+          }),
+        );
+      });
+    });
+  }, 10000);
 });
