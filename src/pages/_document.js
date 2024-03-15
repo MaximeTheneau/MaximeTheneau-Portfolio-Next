2ee7e635efc3e6b8751cda2aaaec234d240a2ba4
Theneau Maxime <theneau.maxime@gmail.com>
2024-03-15 11:30:42 +0100

    Feat: create document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,16 @@
+import {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+
+    </Html>
+  );
+}
