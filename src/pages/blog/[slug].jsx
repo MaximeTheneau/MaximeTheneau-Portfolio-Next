50c6163cb67643de9852f46b0c58e8e0530df398
Theneau Maxime <theneau.maxime@gmail.com>
2024-11-14 15:15:47 +0100

    Feat: add categories for blog

---
+++ src/pages/blog/[slug].jsx
@@ -0,0 +1,74 @@
+import Image from 'next/image';
+import ArticleJsonLd from '../../components/jsonLd/ArticleJsonLd';
+import Page404 from '../404';
+import ImageLoaderFull from '../../utils/ImageLoaderFull';
+import HeadComponents from '../../components/head/HeadComponents';
+import Button from '../../components/button/Button';
+
+export async function getStaticPaths() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}posts&category=blog`);
+  const posts = await res.json();
+
+  const paths = posts.map((post) => ({ params: { slug: post.slug } }));
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}posts/${params.slug}`);
+  const post = await res.json();
+
+  return { props: { post } };
+}
+
+export default function Slug({ post }) {
+  if (!post) return <Page404 />;
+  return (
+    <>
+      <HeadComponents
+        title={post.heading}
+        description={post.metaDescription}
+        image={post.image}
+        url={post.url}
+      />
+      <ArticleJsonLd post={post} />
+      <article className="m-4">
+        <Image
+          src={`${post.slug}.webp`}
+          alt={post.altImg || post.title}
+          loader={ImageLoaderFull}
+          quality={100}
+          width="1080"
+          height="720"
+          sizes="(max-width: 768px) 100vw,
+            (max-width: 1200px) 50vw,
+            33vw"
+        />
+
+        <h1>{post.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.contents }} />
+        {post.paragraphPosts.map((paragraphPosts) => (
+          <div key={paragraphPosts.subtitle}>
+            <h2>{paragraphPosts.subtitle}</h2>
+            <div className="w-responsive" dangerouslySetInnerHTML={{ __html: paragraphPosts.paragraph }} />
+          </div>
+        ))}
+        <div>
+          {post.github && (
+            <Button
+              text="Dépôt GitHub"
+              link={post.github}
+              icon="icon-github"
+            />
+          )}
+          {post.website && (
+            <Button
+              text="Site web"
+              link={post.website}
+              icon="icon-paper-plane"
+            />
+          )}
+        </div>
+      </article>
+    </>
+  );
+}
---
+++ src/pages/blog/index.jsx
@@ -0,0 +1,37 @@
+import HeadComponents from '../../components/head/HeadComponents';
+import Cards from '../../components/cards/Cards';
+
+export async function getStaticProps() {
+  const responseArticles = await fetch(`${process.env.NEXT_PUBLIC_API_URL}posts&category=blog`);
+  const responsePage = await fetch(`${process.env.NEXT_PUBLIC_API_URL}posts/blog`);
+
+  const articles = await responseArticles.json();
+  const page = await responsePage.json();
+
+  return {
+    props: {
+      articles,
+      page,
+    },
+  };
+}
+
+export default function Home({ articles, page }) {
+  return (
+    <>
+      <HeadComponents
+        title={page.heading}
+        description={page.metaDescription}
+        image={page.image}
+        url={page.url}
+      />
+      <section className="m-4">
+        <h1>{page.title}</h1>
+        <p>
+          {page.contents}
+        </p>
+        <Cards cards={articles} path="Creation" />
+      </section>
+    </>
+  );
+}
