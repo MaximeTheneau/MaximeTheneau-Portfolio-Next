0835fbc97a406d6acd7ae921d8fcd689a1bf4b5a
Theneau Maxime <theneau.maxime@gmail.com>
2024-11-26 18:57:39 +0100

    Feat: change font

--- src/components/faq/FaqElements.tsx
+++ src/components/faq/FaqElements.tsx
@@ -43,15 +43,14 @@ function FaqElements({ faq, setFaqs }: FaqElementsProps) {
         }}
         aria-expanded={faq.open}
         aria-controls={`faq-${faq.id}`}
+        className="w-full flex items-center justify-between pt-4 pb-4 border-b-2 "
       >
-        <h3 className="w-full flex items-center justify-between pt-4 pb-4 border-b-2 ">
-          <span id={`faq-${faq.id}`} className="underline">{faq.title}</span>
-          <button type="button" aria-label={faq.open ? 'Fermer la réponse' : 'Ouvrir la réponse'} className="pr-2">
-            {faq.open ? <i className="icon-x" /> : <i className="icon-scroll" />}
-          </button>
-        </h3>
-        <div className={faq.open || !bot ? 'block' : 'hidden'} dangerouslySetInnerHTML={{ __html: faq.description }} />
+        <span id={`faq-${faq.id}`} className="underline block">{faq.title}</span>
+        <span aria-label={faq.open ? 'Fermer la réponse' : 'Ouvrir la réponse'} className="pr-2">
+          {faq.open ? <i className="icon-x" /> : <i className="icon-scroll" />}
+        </span>
       </button>
+      <div className={faq.open || !bot ? 'block' : 'hidden'} dangerouslySetInnerHTML={{ __html: faq.description }} />
     </li>
   );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,24 +1,35 @@
+import { Jura } from 'next/font/google';
 import Cursor from './cursor/Cursor';
 import Navbar from './navbar/Navbar';
 import Footer from './footer/Footer';
 
 type LayoutProps = {
   children: React.ReactNode;
-  }
+}
+const inter = Jura({ subsets: ['latin'] });
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex flex-col h-screen">
-      <header className="min-h-10">
-        <Navbar />
-      </header>
-      <main className="flex-grow">
-        {children}
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-      <Cursor />
-    </div>
+    <>
+      <style jsx global>
+        {`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}
+      </style>
+      <div className="flex flex-col h-screen">
+        <header className="min-h-10">
+          <Navbar />
+        </header>
+        <main className="flex-grow">
+          {children}
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+        <Cursor />
+      </div>
+    </>
   );
 }
