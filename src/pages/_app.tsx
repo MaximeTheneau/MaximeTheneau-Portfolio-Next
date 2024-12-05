ef419d5e890ac07b82d8a5e5daab073b043ac09f
Theneau Maxime <theneau.maxime@gmail.com>
2024-12-05 13:30:45 +0100

    Feat: optimize perf

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,18 @@
 import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
 import Layout from '../components/layout';
 import '../styles/globals.scss';
-import { CookiesProvider } from '../context/CookiesContext';
-import CookiesModal from '../components/modal/Cookies';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const CookiesModal = dynamic(() => import('@/components/modal/Cookies'), { ssr: false });
+
   return (
-    <CookiesProvider>
+    <>
       <CookiesModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </CookiesProvider>
+    </>
 
   );
 }
