50ed5f6eedd74e1d5fa4f02e9420c8fd1a9c140e
Theneau Maxime <theneau.maxime@gmail.com>
2025-01-20 18:34:30 +0100

    Feat: jsonLd for A-propos page

--- src/components/jsonLd/ArticleJsonLd.jsx
+++ src/components/jsonLd/ArticleJsonLd.jsx
@@ -1,10 +1,11 @@
-import { url } from 'inspector';
 import Head from 'next/head';
 
 export default function ArticleJsonLd({ post, urlPost }) {
   const jsonLdData = {
     '@context': 'https://schema.org/',
-    '@type': 'Article',
+    '@type': 'BlogPosting',
+    '@id': `${process.env.NEXT_PUBLIC_URL}/blog`,
+    mainEntityOfPage: `${process.env.NEXT_PUBLIC_URL}/blog`,
     name: post.title,
     headline: post.title,
     description: post.metaDescription,
@@ -15,11 +16,27 @@ export default function ArticleJsonLd({ post, urlPost }) {
     isAccessibleForFree: 'True',
     keywords: `${post.title}, ${post.category.name}${post.subcategory ? `, ${post.subcategory.name}` : ''}`,
     articleBody: post.contents,
-    image: `${post.imgPost}?format=jpeg`,
+    image: {
+      '@type': 'ImageObject',
+      '@id': `${post.imgPost}?format=jpeg`,
+      url: `${post.imgPost}?format=jpeg`,
+      height: post.imgPost.height,
+      width: post.imgPost.width,
+    },
     author: {
       '@type': 'Person',
       name: 'Maxime THENEAU',
-      url: 'https://maximefreelance.fr/A-propos',
+      url: `${process.env.NEXT_PUBLIC_URL}/A-propos`,
+    },
+    isPartOf: {
+      '@type': 'Blog',
+      '@id': `${process.env.NEXT_PUBLIC_URL}/blog/${post.category.slug}`,
+      name: 'Maxime Freelance Blog ',
+      publisher: {
+        '@type': 'Organization',
+        '@id': `${process.env.NEXT_PUBLIC_URL}`,
+        name: 'Maxime Freelance',
+      },
     },
     publisher: {
       '@type': 'Organization',
@@ -29,6 +46,18 @@ export default function ArticleJsonLd({ post, urlPost }) {
         url: 'https://picture.theneaumaxime.fr/Portfolio-4.webp?format=jpeg',
       },
     },
+    commentCount: post.comments.length,
+    comment: [
+      post.comments.map((comment) => ({
+        '@type': 'Comment',
+        author: {
+          '@type': 'Person',
+          name: comment.User,
+        },
+        description: comment.comment,
+        dateCreated: comment.createdAt,
+      })),
+    ],
   };
 
   return (
