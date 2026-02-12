426b976f8dfd337fa0e1d97d20761db9c0595c05
Maxime Theneau <theneau.maxime@gmail.com>
2026-02-12 16:15:13 +0100

    Chlore: Next.js pnpm audit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   sassOptions: {
     silenceDeprecations: ['legacy-js-api'],
   },
